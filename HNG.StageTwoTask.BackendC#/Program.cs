52c7dbdfbe71922801213551ed7b2cbaa64db007
Anthonia Okwe <anthonia_okwe@yahoo.com>
2024-07-09 19:16:41 +0100

    .

--- HNG.StageTwoTask.BackendC#/Program.cs
+++ HNG.StageTwoTask.BackendC#/Program.cs
@@ -18,7 +18,10 @@ builder.Services.AddTransient<ITokenService, TokenService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Organisation API", Version = "v1" });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -74,11 +77,15 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Organisation API V1");
+        c.RoutePrefix = string.Empty;
+    });
+//}
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
